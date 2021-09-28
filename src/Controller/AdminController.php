2c72fb19b33ba02fcb7ee61da98edcb70f92caab
Ramon Jansen <r.b.jansen03@gmail.com>
2021-09-28 11:27:42 +0200

    terminal pushed

---
+++ src/Controller/AdminController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use App\Entity\Author;
+use App\Form\AuthorFormType;
+
+/** @var EntityManagerInterface */
+private $entityManager;
+
+/** @var \Doctrine\Common\Persistence\ObjectRepository */
+private $authorRepository;
+
+/** @var \Doctrine\Common\Persistence\ObjectRepository */
+private $blogPostRepository;
+
+/**
+ * @param EntityManagerInterface $entityManager
+ */
+public function __construct(EntityManagerInterface $entityManager)
+{
+    $this->entityManager = $entityManager;
+    $this->blogPostRepository = $entityManager->getRepository('App:BlogPost');
+    $this->authorRepository = $entityManager->getRepository('App:Author');
+}
+
+class AdminController extends AbstractController
+{
+    #[Route('/admin', name: 'admin')]
+    public function index(): Response
+    {
+        return $this->render('admin/index.html.twig', [
+            'controller_name' => 'AdminController',
+        ]);
+    }
+}
