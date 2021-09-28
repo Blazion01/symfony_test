cb0401ca89f342927a4dc33f74bc3e7025a73161
Ramon Jansen <r.b.jansen03@gmail.com>
2021-09-28 13:10:09 +0200

    terminal pushed

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,18 +2,19 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Component\HttpFoundation\Request;
-use App\Entity\Author;
-use App\Form\AuthorFormType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-//use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Annotation\Route;
 
 class AdminController extends AbstractController
 {
+    use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+    use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+    use Doctrine\ORM\EntityManagerInterface;
+    use Symfony\Component\HttpFoundation\Request;
+    use App\Entity\Author;
+    use App\Form\AuthorFormType;
+
     /** @var EntityManagerInterface */
     private $entityManager;
     
