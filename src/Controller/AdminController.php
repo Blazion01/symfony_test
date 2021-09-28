6d0d831e43d8d5e67fd737e28b6f362d8b3f3677
Ramon Jansen <r.b.jansen03@gmail.com>
2021-09-28 12:59:18 +0200

    terminal pushed

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,37 +2,37 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use App\Entity\Author;
 use App\Form\AuthorFormType;
-
-/** @var EntityManagerInterface */
-private $entityManager;
-
-/** @var \Doctrine\Persistence\ObjectRepository */
-private $authorRepository;
-
-/** @var \Doctrine\Persistence\ObjectRepository */
-private $blogPostRepository;
-
-/**
- * @param EntityManagerInterface $entityManager
- */
-public function __construct(EntityManagerInterface $entityManager)
-{
-    $this->entityManager = $entityManager;
-    $this->blogPostRepository = $entityManager->getRepository('App:BlogPost');
-    $this->authorRepository = $entityManager->getRepository('App:Author');
-}
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+//use Symfony\Component\Routing\Annotation\Route;
 
 class AdminController extends AbstractController
 {
+    /** @var EntityManagerInterface */
+    private $entityManager;
+    
+    /** @var \Doctrine\Persistence\ObjectRepository */
+    private $authorRepository;
+    
+    /** @var \Doctrine\Persistence\ObjectRepository */
+    private $blogPostRepository;
+    
+    /**
+     * @param EntityManagerInterface $entityManager
+     */
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+        $this->blogPostRepository = $entityManager->getRepository('App:BlogPost');
+        $this->authorRepository = $entityManager->getRepository('App:Author');
+    }
+
     #[Route('/admin', name: 'admin')]
     public function index(): Response
     {
