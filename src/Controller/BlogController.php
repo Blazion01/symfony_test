ff41af2a5fc3eeb474b5d76159f7f38cc6cb6b52
Ramon Jansen <r.b.jansen03@gmail.com>
2021-09-29 12:23:22 +0200

    terminal pushed

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -4,15 +4,42 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+//use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
 class BlogController extends AbstractController
 {
-    #[Route('/', name: 'homepage')]
+    /** @var EntityManagerInterface */
+    private $entityManager;
+
+    /** @var \Doctrine\Common\Persistence\ObjectRepository */
+    private $authorRepository;
+
+    /** @var \Doctrine\Common\Persistence\ObjectRepository */
+    private $blogPostRepository;
+
+    /**
+     * @param EntityManagerInterface $entityManager
+     */
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+        $this->blogPostRepository = $entityManager->getRepository('App:BlogPost');
+        $this->authorRepository = $entityManager->getRepository('App:Author');
+    }
+
+    //#[Route('/', name: 'homepage')]
+    // #[Route('/entries', name: 'entries')]
+    /**
+     * @Route('/', name: 'homepage')
+     * @Route('/entries', name: 'entries')
+     */
     public function indexAction(): Response
     {
-        return $this->render('blog/index.html.twig', [
-            'controller_name' => 'BlogController',
+        return $this->render('blog/entries.html.twig', [
+            'blogPosts' => $this->blogPostRepository->findAll()
         ]);
     }
 }
